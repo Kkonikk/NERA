e372e6448509064b9681c39e6b6c0a93b06c583e
karpenka <osnerde@gmail.com>
2018-12-04 18:31:03 +0300

    opt mod

--- temp/opt_w.m
+++ temp/opt_w.m
@@ -1,31 +1,36 @@
 model = mccode('NERA_ell_guide.instr');
 
 
-for w = 0.01:0.01:0.1
+for h = 0.01:0.02:0.31
     fix(model, 'all');
 
-    model.sample_dist = 0.35;
-    model.sample_size = 0.03;
+	model.sample_size = 0.03;
 
-    model.guide_length = 107;
-    model.guide_width = w;
-    model.guide_height=0.14;
-    model.source_optics_dist = 5.61;
-    source_pulse_number=2;
+	model.guide_width=0.1;
+	model.source_optics_dist = 5.61;
+	source_pulse_number=2;
 
-    model.source_lambda_min = 1.0;
-    model.source_lambda_max = 5.0;
+	model.source_lambda_min = 1.0;
+	model.source_lambda_max = 5.0;
 
-    model.nose_length = 'free'; model.nose_length = [0.5 4.8 20];
-    model.loutw = 'free'; model.loutw = [0 0.38 5];
-    model.linw = 'free'; model.linw = [0 12.3 20];
-    model.linh = 0;
-    model.louth = 0;
+	%model.loutw = 'free'; model.loutw = [0 0.38 5];
+	%model.linw = 'free'; model.linw = [0 5 10];
+	model.loutw=0;
+	model.linw=0;
+	model.guide_height = h;
+	%model.guide_height = 'free'; model.guide_height = [0.01 0.2 0.3];
+	model.linh = 'free'; model.linh = [0 10 20];
+	model.louth = 'free'; model.louth = [0 0.35 5];
 
-    [parameters, fval, status, output]=fmax(model,[], ...
-    'optimizer=fminpso; OutputFcn=fminplot;TolFun =5%;TolX=5%;ncount=1e5;MaxFunEvals=100', nan);
+	[parameters, fval, status, output]=fmax(model,[], ...
+	'optimizer=fminpso; OutputFcn=fminplot;TolFun =5%;TolX=5%;ncount=1e5;MaxFunEvals=100', nan);
+
+	output.parsBest
+	a = model(parameters,nan);
+	a1 = iData(a);
+	a2 = sum(a1,0);
 
     A(i) = model(parameters, nan);
-    I(i) = sum(iData(A(i)),0);
+    I(i) = a2;
     i = i+1;
 end
